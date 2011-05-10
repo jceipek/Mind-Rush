6c9e676c10d44b243ea325455af54f59c7ba8196
Julian Ceipek <julian.ceipek@gmail.com>
2011-05-10 14:17:46 -0400

    Added old README and the new circuit diagram

--- mindRush.py
+++ mindRush.py
@@ -24,9 +24,19 @@ window = Window(resolution,
 manager = Manager()
 window.registerManager(manager)
 
-arduinoInput = Biofeedback('/dev/tty.usbmodem621',
-                            mindFlexActive=False, eyeCircuitActive=True)
-window.addInputDevice(arduinoInput)
+#-------------------------------------------------------------------------
+#         Uncomment the lines below to obtain bio-feedback control
+#         You may need to modify '/dev/tty.usbmodem621' to match
+#         the location of the Arduino communication port on your
+#         machine. On Windows, this will be 'COM3' or higher,
+#         and on Linux, it will be similar to '/dev/ttyAMC0'.
+#         For instructions on obtaining this string, look through
+#         http://arduino.cc/en/Guide/HomePage
+#-------------------------------------------------------------------------
+#arduinoInput = Biofeedback('/dev/tty.usbmodem621',
+#                            mindFlexActive=False, eyeCircuitActive=True)
+#window.addInputDevice(arduinoInput)
+#-------------------------------------------------------------------------
 
 userInterface = UI(manager)
 #screens must be initialized after the manager and the ui
